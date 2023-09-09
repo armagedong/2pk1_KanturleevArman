96786c0f3f12e40676cd5cf1b89af6caea51e677
User <User@LAPTOP-HBQD9H24>
2023-09-09 13:58:26 +0500

    Добавьте файлы проекта.

---
+++ pz_01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите числа a");
+                double a = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите число b");
+                double b = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите число c");
+                double c = double.Parse(Console.ReadLine());
+                double part1 = (1.5 * Math.Pow(a - b, 2)) / (Math.Abs(a - b) * c);
+                double part2 = Math.Sqrt(Math.Abs(a - b)) * Math.Pow(10, 3);
+                double part3 = 2.5 * (a * a + 2.75) * Math.Sin(-2 * a) / (3 + a * a * b * c);
+                int res = (int)part1 + (int)part2 - (int)part3;
+                Console.WriteLine($"Результат:\t{res}");
+            }
+        }
+    }
+}
