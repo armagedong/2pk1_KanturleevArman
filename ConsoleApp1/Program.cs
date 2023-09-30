5863d52568b458d15020d262c19589002cb7ac82
armankanturleev6551@gmail.com <armankanturleev6551@gmail.com>
2023-09-30 09:37:29 +0300

    добавлена 8 практика

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Генерация длины второго измерения
+Random random = new Random();
+int length = random.Next(6, 21);
+
+// Создание ступенчатого массива
+string[][] array = new string[9][];
+
+// Заполнение массива случайными строками
+for (int i = 0; i < 9; i++)
+{
+    array[i] = new string[length];
+    for (int j = 0; j < length; j++)
+    {
+        array[i][j] = RandomString(5); // Генерация случайной строки
+    }
+}
+
+// Вывод массива на экран
+foreach (string[] row in array)
+{
+    Console.WriteLine(string.Join(", ", row));
+}
+    
+
+    // Метод для генерации случайной строки указанной длины
+    static string RandomString(int length)
+{
+    const string chars = "abcdefghijklmnopqrstuvwxyz";
+    Random random = new Random();
+
+    return new string(Enumerable.Repeat(chars, length)
+      .Select(s => s[random.Next(s.Length)]).ToArray());
