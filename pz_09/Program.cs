bf4c15826a4947bf0b614102e31d286ffa47fba4
armankanturleev6551@gmail.com <armankanturleev6551@gmail.com>
2023-11-18 17:37:45 +0500

    Доьавлена 9 практика

---
+++ pz_09/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите вашу строчку:\t");
+string str = Console.ReadLine();
+str = str.Replace(" ", "");//из строчки удаляются символы пунктуации
+str = str.Replace(".", "");
+str = str.Replace(",", "");
+str = str.Replace(":", "");
+str = str.Replace(";", "");
+char[] array = str.ToCharArray();//в массив записываются отдельные символы строчки
+char[] array2 = array;
+if (array.Reverse().SequenceEqual(array2))//улсловны оператор, который переворацичвает массив и сравнивает с исходной строчкой
+    Console.WriteLine("Слово палиндром");
+else
+    Console.WriteLine("Слово не палиндром");
