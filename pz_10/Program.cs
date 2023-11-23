9367e4ac837efdf4ac2e84c725a632a7047a19e6
armankanturleev6551@gmail.com <armankanturleev6551@gmail.com>
2023-11-23 19:22:03 +0500

    Добавлена 10 практика

---
+++ pz_10/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Write("Введите первую строчку:\t");
+string str1 = Console.ReadLine();
+Console.Write("Введите вторую строчку:\t");
+string str2 = Console.ReadLine();
+string[] text1 = str1.Split();
+string[] text2 = str2.Split();
+string end = "";
+
+if (text1.Length < text2.Length)
+{
+    for (int i = 0; i < text1.Length; i++)
+    {
+        end += text1[i] + " " + text2[i] + " ";
+    }
+}
+else
+{
+    for (int i = 0; i < text2.Length; i++)
+    {
+        end += text1[i] + " " + text2[i] + " ";
+    }
+}
+
+if (text1.Length > text2.Length)
+{
+    for (int i = text2.Length; i < text1.Length; i++)
+    {
+        end += text1[i] + " ";
+    }
+}
+else
+{
+    for (int i = text1.Length; i < text2.Length; i++)
+    {
+        end += text2[i] + " ";
+    }
+}
+Console.WriteLine(end);
