be778ed4c9f9bbd3278d3c70fc49bb0751e6176f
armankanturleev6551@gmail.com <armankanturleev6551@gmail.com>
2023-12-01 14:22:46 +0500

    Добавлена 12 практика

---
+++ pz_12/Program.cs
@@ -0,0 +1,31 @@
+﻿
+string formula = Console.ReadLine();
+
+Proverka(formula);
+
+static void Proverka(string str)
+{
+    char[] strings = str.ToCharArray();
+    int count1 = 0;
+    int count2 = 0;
+    foreach (char c in strings)
+    {
+        if (c == ')')
+        {
+            count1++;
+        }
+        else if (c == '(')
+        {
+            count2++;
+        }
+    }
+
+    if(count1 == count2 && count2 + count1 != strings.Length)
+    {
+        Console.WriteLine(true); ;
+    }
+    else
+    {
+        Console.WriteLine(false);
+    }
+}
